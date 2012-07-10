fd467f44ab5c7c8ece7e8dbfcb9b9c72343a7634
Travis Madar <tmadar@Karls-MacBook-Air.local>
2012-07-10 12:40:24 -0700

    some validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
+    validates :title, uniqueness: true
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :image_url, allow_blank: true, format:
+    {
+    with:       %r{\.(gif|png|jpg)$}i,
+    message:    'must be a URL for GIF, JPG, or PNG image'
+    }
   attr_accessible :description, :image_url, :price, :title
 end
